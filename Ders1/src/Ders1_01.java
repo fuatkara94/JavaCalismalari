ea5fd518bb0ba9099f449f59e70c889f9d928fa9
User <User@10.50.13.123>
2014-12-12 15:22:38 +0200

    ilk yükleme

---
+++ Ders1/src/Ders1_01.java
@@ -0,0 +1,47 @@
+import com.sun.java_cup.internal.runtime.Scanner;
+
+class Dort_islem{
+	double a , b ;
+	double sonuc;
+	public void setSayi()
+	{
+		this.a = a;
+		this.b = b;
+	}
+	public double toplama(double a ,double b)
+	{
+		sonuc= a + b ;
+		return sonuc ;
+	}
+	public double cikarma(double a ,double b)
+	{
+		sonuc= a - b ;
+		return sonuc ;
+	}
+	public double carpma(double a ,double b)
+	{
+		sonuc= a * b ;
+		return sonuc ;
+	}
+	public double bolme(double a ,double b)
+	{
+		sonuc= a / b ;
+		return sonuc ;
+	}
+	public void getSonuc(){
+		System.out.println("Ýþlemin sonucu: " + sonuc);
+	}
+}
+
+public class Ders1_01 {
+
+	public static void main(String[] args) {
+		Dort_islem Toplat = new Dort_islem();
+		System.out.println("Birinci sayiyi giriniz: ");
+		Scanner input1=new 
+		int sayi=input1.nextdouble();
+		
+
+	}
+
+}
